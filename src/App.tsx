e4837e9e8d53fc5f58367f4ed8205142b8c8a97a
Dylan Tran <dylanbt928@gmail.com>
2025-07-03 19:05:06 -0700

    Create hero file

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>
+      <Home />
+    </div>
   );
 };
 
---
+++ src/components/Hero.tsx
@@ -0,0 +1,9 @@
+function Hero() {
+  return (
+    <div>
+      <p>Hero Section</p>
+    </div>
+  );
+}
+
+export default Hero;
